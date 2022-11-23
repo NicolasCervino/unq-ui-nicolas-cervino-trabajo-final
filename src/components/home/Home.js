c71475161c6aa9f24ba1c54ade3ec1eb8cf82d29
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-23 18:32:16 -0300

    Home utiliza renderizado condicional para DifficultySelector o Game segun corresponda

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { GameContext } from "../../context/GameContext";
 import Api from "../../service/api";
 import DifficultySelector from "../difficultySelector/DifficultySelector";
+import Game from "../game/Game";
 
 const Home = () => {
     const [difficulties, setDifficulties] = useState(null);
+    const { selectedDifficulty } = useContext(GameContext);
 
     useEffect(() => {
         Api.getDifficulties().then((response) => {
             setDifficulties(response.data);
         });
     }, []);
+
+    if (selectedDifficulty) {
+        return <Game></Game>;
+    }
 
     return difficulties && <DifficultySelector difficulties={difficulties} />;
 };

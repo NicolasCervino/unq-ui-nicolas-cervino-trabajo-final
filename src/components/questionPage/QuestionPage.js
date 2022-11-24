fd4fc1d2019b4e624a2633973e92ab1293d27d2c
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-24 00:20:03 -0300

    QuestionPage ahora solo se encarga de setFinished y setCurrentQuestion y delega trabajo a QuestionOptions

--- src/components/questionPage/QuestionPage.js
+++ src/components/questionPage/QuestionPage.js
@@ -1,29 +1,21 @@
 import { GameContext } from "../../context/GameContext";
 import { useState, useEffect, useContext } from "react";
+import QuestionOptions from "./questionOptions/QuestionOptions";
 import "./question-page.css";
 
 const QuestionPage = () => {
-    const { questions, answered, setFinished, setAnswered, selectedDifficulty } = useContext(GameContext);
-    const [currentQuestion, setCurrentQuestion] = useState("");
+    const { questions, answered, setFinished, selectedDifficulty } = useContext(GameContext);
     const [index, setIndex] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(questions[index]);
 
     useEffect(() => {
         if (questions.length === answered.length) {
             setFinished(true);
         }
-    }, [answered]);
-
-    useEffect(() => {
         if (index < questions.length) {
             setCurrentQuestion(questions[index]);
         }
-    }, [questions, index]);
-
-    const handleNext = () => {
-        if (questions.length > index + 1) {
-            setIndex((prevState) => prevState + 1);
-        }
-    };
+    }, [answered, index, questions, setFinished]);
 
     return (
         <div className="row justify-content-center question-page">
@@ -33,17 +25,9 @@ const QuestionPage = () => {
                 <p className="">{currentQuestion.question}</p>
             </div>
 
-            <div className="col-10 d-flex flex-column justify-content-center gap-3 my-3">
-                <button className="btn btn-light">{currentQuestion.option1}</button>
-                <button className="btn btn-light">{currentQuestion.option2}</button>
-                <button className="btn btn-light">{currentQuestion.option3}</button>
-                <button className="btn btn-light">{currentQuestion.option4}</button>
-            </div>
-            <div className="col-10 mt-1 justify-self-end">
-                <button className="btn btn-success" onClick={handleNext}>
-                    Next
-                </button>
+            <QuestionOptions question={currentQuestion} setIndex={setIndex} setCurrentQuestion={setCurrentQuestion} />
 
+            <div className="col-10 mt-1 justify-self-end text-white">
                 <p>{index + 1 + "/" + questions.length}</p>
             </div>
         </div>

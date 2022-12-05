464eb541e4b3e458fa53965466bb1e5b9b8ca5d4
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-04 23:39:28 -0300

    se agregan rutas al proyecto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Home from "./components/home/Home";
+import Game from "./components/game/Game";
+import DifficultySelector from "./components/difficultySelector/DifficultySelector";
 import { GameProvider } from "./context/GameContext";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <GameProvider>
                 <main className="container-fluid">
-                    <Home></Home>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/game" element={<Game />} />
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
                 </main>
             </GameProvider>
         </div>

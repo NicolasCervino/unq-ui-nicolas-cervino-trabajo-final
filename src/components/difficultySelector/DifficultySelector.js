8155e7780d17d337ec270a3427b2549dcd68ddd7
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-04 23:54:54 -0300

    El boton de play ahora es un link a /game

--- src/components/difficultySelector/DifficultySelector.js
+++ src/components/difficultySelector/DifficultySelector.js
@@ -1,7 +1,8 @@
 import { useState, useContext } from "react";
 import DifficultyButton from "./difficultyButton/DifficultyButton";
-import "./difficulty-selector.css";
 import { GameContext } from "../../context/GameContext";
+import { Link } from "react-router-dom";
+import "./difficulty-selector.css";
 
 const DifficultySelector = ({ difficulties }) => {
     const [currentDifficulty, setCurrentDifficulty] = useState(null);
@@ -18,16 +19,14 @@ const DifficultySelector = ({ difficulties }) => {
     };
 
     return (
-        <div className="difficulty-selector--container row">
+        <div className="difficulty-selector--container col-12">
             <div className="col-10 col-lg-6 d-flex flex-column justify-content-center gap-3 my-3">
                 {difficulties.map((d) => (
                     <DifficultyButton key={d} difficulty={d} onClickAction={handleDifficultySelect} active={currentDifficulty === d} />
                 ))}
-            </div>
-            <div className="col-10 col-lg-6 d-flex flex-column">
-                <button className="btn btn-primary m-3 mt-5" disabled={currentDifficulty === null} onClick={handlePlay}>
-                    Play!
-                </button>
+                <Link to={"/game"} className={`btn btn-play m-3 mt-5 ${!currentDifficulty ? "disabled" : ""}`} onClick={handlePlay}>
+                    PLAY
+                </Link>
             </div>
         </div>
     );
